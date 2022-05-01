edfe001136f52837fad13749603f9b86b3940467
William Lin <wlin26@yahoo.com>
2022-05-01 17:43:55 -0400

    Updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
-    plugins: [
-      {
-        resolve: "@wkocjan/gatsby-theme-intro",
-        options: {
-          theme: "classic",
-        },
+  siteMetadata: {
+    description: "Personal Site for William Lin",
+    locale: "en",
+    title: "William Lin",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "dark-green",
+        contentPath: "content/",
+        showThemeLogo: true,
       },
-    ],
+    },
+  ],
 }
